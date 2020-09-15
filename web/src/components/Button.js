91d278ed789079d0ae3f831f0ae9964b9d786bde
JunKyus <kuk941025@gmail.com>
2020-09-16 01:26:58 +0900

    Use classnames to handle class-related props

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 /**
  * @param {props} param0
@@ -6,13 +7,16 @@ import React from 'react';
  * type: 버튼 유형. primary, secondary, success, danger, warning, info, light, dark, link
  * disabled: 클릭 가능 여부
  * size: 크기. lg, sm.
- * 
+ *
  * @todo icon props로 받기
  */
 
 const Button = ({ children, type = 'primary', disabled = false, size = '', onClick = null }) => {
   return (
-    <button onClick={onClick} disabled={disabled} className={`btn btn-${type} btn-${size}`}>
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={classNames('btn', `btn-${type}`, size !== '' && `btn-${size}`)}>
       {children}
     </button>
   );
