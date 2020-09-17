4f6ef6831350fdd177d4b891863c103c793fde5d
yoonseo <nicealice99@gmail.com>
2020-09-17 15:56:40 +0900

    first commit

---
+++ web/src/pages/Create.js
@@ -0,0 +1,14 @@
+import CreateLink from '@/components/CreateLink';
+import React, { Component } from 'react';
+
+class Create extends Component {
+  render() {
+    return (
+      <div>
+        <CreateLink></CreateLink>
+      </div>
+    );
+  }
+}
+
+export default Create;
