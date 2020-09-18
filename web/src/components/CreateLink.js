d5f6a1b1da891e291565db186213b5794a9f68cd
yoonseo <nicealice99@gmail.com>
2020-09-19 04:03:16 +0900

    third commit

--- web/src/components/CreateLink.js
+++ web/src/components/CreateLink.js
@@ -6,7 +6,6 @@ const CreateLink = () => {
   const [inputs, setInputs] = useState({
     lecture: '',
     subject: '',
-    isTimeValid: false,
   });
 
   //수업명과 과목명 입력받기
@@ -19,7 +18,7 @@ const CreateLink = () => {
     });
   };
 
-  //시간 입력받기(숫자만 입력받을 수 있도록 따로 만들었습니다)
+  //시간 입력받기(숫자만 입력받을 수 있도록 따로 분리)
   const handleTimeValueChange = (e) => {
     const re = /^[0-9\b]+$/;
     if (e.target.value === '' || re.test(e.target.value)) {
@@ -30,8 +29,12 @@ const CreateLink = () => {
   //버튼 누르면 값을 콘솔창에서 확인할 수 있도록
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`${lecture} ${subject} ${time}`);
-    onReset();
+    if (`${lecture}` == '' || `${subject}` == '' || `${time}` == '') {
+      alert('내용을 입력하세요');
+    } else {
+      console.log(`${lecture} ${subject} ${time}`);
+      onReset();
+    }
   };
 
   //버튼 누른 후 입력창 초기화 되도록
@@ -44,67 +47,57 @@ const CreateLink = () => {
   };
 
   return (
-    <nav className="navbar navbar-light bg-light">
-      <form className="form-inline" onSubmit={handleSubmit}>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              클래스명 입력
-            </span>
+    <div className="divStyle">
+      <nav className="navbar navbar-light bg-light">
+        <form className="form-inline" onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="text-group">클래스명 입력</div>
+            </div>
+            <input type="text" className="form-control" name="lecture" onChange={handleValueChange} value={lecture} />
           </div>
-          <input type="text" className="form-control" name="lecture" onChange={handleValueChange} value={lecture} />
-        </div>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              종목 입력
-            </span>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="text-group">종목 입력</div>
+            </div>
+            <input type="text" className="form-control" name="subject" onChange={handleValueChange} value={subject} />
           </div>
-          <input type="text" className="form-control" name="subject" onChange={handleValueChange} value={subject} />
-        </div>
-        <div className="input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon1">
-              시간 입력(분)
-            </span>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="text-group">시간(분) 입력</div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-field"
+              name="time"
+              onChange={handleTimeValueChange}
+              value={time}
+            />
           </div>
-          <input
-            type="text"
-            className="form-control form-third"
-            name="time"
-            onChange={handleTimeValueChange}
-            value={time}
-          />
-        </div>
-        <button type="submit" className="btn btn-secondary">
-          링크생성
-        </button>
-      </form>
+          <button type="submit" className="btn btn-secondary">
+            링크생성
+          </button>
+        </form>
+      </nav>
+
       <style jsx>
         {`
-          .form-third {
-            width: 144px;
+          .text-group {
+            margin: 5px 10px 5px 100px;
           }
-          .navbar-light {
-            width: 100%;
-            color: red;
+          button {
+            margin-left: 100px;
+            border-radius: 50px;
+          }
+          .text-field {
+            width: 100px;
+          }
+          nav {
+            display: flex;
           }
         `}
       </style>
-    </nav>
-
-    // <nav className="navbar navbar-light bg-light">
-    //   <form onSubmit={handleSubmit} className="form-inline">
-    //     <div className="input-group">
-    //       <input type="text" className="myInput" name="lecture"  onChange={handleValueChange} value={lecture} />
-    //       <input type="text" className="myInput"name="subject" onChange={handleValueChange} value={subject} />
-    //       <input type="text" className="myInput"name="time" onChange={handleTimeValueChange} value={time} />
-    //       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-    //         링크생성
-    //       </button>
-    //     </div>
-    //   </form>
-    // </nav>
+    </div>
   );
 };
 
