3dd5e6828cf0234f8c27a415955cd26489ffceae
JunKyus <kuk941025@gmail.com>
2020-09-22 14:22:52 +0900

    Install prop-types

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-/**
- * @param {props} param0
- * children: text
- * type: 버튼 유형. primary, secondary, success, danger, warning, info, light, dark, link
- * disabled: 클릭 가능 여부
- * size: 크기. lg, sm.
- *
- * @todo icon props로 받기
- */
-
-const Button = ({ children, type = 'primary', disabled = false, size = '', onClick = null }) => {
+const Button = ({ children, type = 'primary', disabled = false, size = 'md', onClick = null }) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={classNames('btn', `btn-${type}`, size !== '' && `btn-${size}`)}>
+      className={classNames('btn', `btn-${type}`, size !== 'md' && `btn-${size}`)}>
       {children}
     </button>
   );
 };
 
+Button.propTypes = {
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']),
+  disabled: PropTypes.bool,
+  size: PropTypes.oneOf(['lg', 'sm', 'md']),
+  onClick: PropTypes.func,
+};
 export default Button;
