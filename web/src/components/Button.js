31118050f50a7109536edbe3b22b1caaecc82e91
JunKyus <kuk941025@gmail.com>
2020-09-22 17:17:26 +0900

    Update button props: color and type

--- web/src/components/Button.js
+++ web/src/components/Button.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Button = ({ children, type = 'primary', disabled = false, size = 'md', onClick = null }) => {
+const Button = forwardRef((props, ref) => {
+  const {
+    children,
+    type = 'button',
+    color = 'primary',
+    disabled = false,
+    size = 'md',
+    onClick = null,
+    roundType = 'rounded-0',
+  } = props;
   return (
     <button
+      ref={ref}
+      type={type}
       onClick={onClick}
       disabled={disabled}
-      className={classNames('btn', `btn-${type}`, size !== 'md' && `btn-${size}`)}>
+      className={classNames('btn', `btn-${color}`, size !== 'md' && `btn-${size}`, roundType)}>
       {children}
     </button>
   );
-};
+});
 
 Button.propTypes = {
   children: PropTypes.node,
-  type: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']),
+  color: PropTypes.oneOf(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark', 'link']),
   disabled: PropTypes.bool,
   size: PropTypes.oneOf(['lg', 'sm', 'md']),
   onClick: PropTypes.func,
+  roundType: PropTypes.oneOf(['rounded', 'rounded-top', 'rounded-circle', 'rounded-pill', 'rounded-0']),
+  type: PropTypes.string,
 };
 export default Button;
