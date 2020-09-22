eb05f7553e19fe0c1ae722cdc20b8f385e2e69aa
timetree <timetree@kakao.com>
2020-09-22 23:06:26 +0900

    /auth 등록, 검증 파트 추가 완료 추후 JWT 라이브러리 도입 예정

---
+++ server/model/mysql/db_info.js
@@ -0,0 +1,18 @@
+module.exports = (function () {
+  return {
+    real: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE
+    },
+    dev: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE
+    }
+  }
+})();
