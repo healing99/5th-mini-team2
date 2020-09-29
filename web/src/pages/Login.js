55b92ece68a90bb6b242815e402501201d0d92e3
healing99 <49135797+healing99@users.noreply.github.com>
2020-09-29 21:41:42 +0900

    Login and Welcome Layout

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -3,6 +3,7 @@ import Button from '@/components/Button';
 import TextField from '@/components/TextField';
 import { REG_EXP } from '@/const/RegExp';
 import TestyImg from '@/assets/images/testy.png';
+import backgroundImg from '@/assets/images/background.png';
 
 const Login = () => {
   const loginButton = useRef();
@@ -30,50 +31,60 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <p>시험관리 온라인 서비스</p>
-      <div className="col" align="center">
-        <img style={{ backgroundColor: 'black', marginBottom: '40px' }} src={TestyImg} />
-        <div className="row">
-          <TextField
-            onChange={handleValueChange}
-            value={inputs.studentName}
-            name="studentName"
-            placeholder="이름"
-            style={{ width: '400px', height: '42px', borderRadius: '22px', margin: 'auto' }}
-          />
-        </div>
-        <div className="row">
-          <TextField
-            onChange={handleValueChange}
-            pattern={REG_EXP.Number}
-            value={inputs.classCode}
-            name="classCode"
-            placeholder="학원코드입력"
-            style={{ width: '400px', height: '42px', borderRadius: '22px', margin: 'auto' }}
-          />
-        </div>
-        <div className="row justify-content-center">
-          <Button
-            onClick={handleSubmit}
-            ref={loginButton}
-            type="submit"
-            style={{
-              width: '400px',
-              height: '42px',
-              backgroundColor: '#e4cd84',
-              border: 'none',
-              borderRadius: '22px',
-            }}>
-            <span style={{ color: '#4ca2c5', fontWeight: 'bold' }}>로그인</span>
-          </Button>
+    <div className="bg">
+      <div className="container">
+        <p>시험관리 온라인 서비스</p>
+        <div className="col" align="center">
+          <img className="testy-img" src={TestyImg} />
+          <div className="row">
+            <TextField
+              onChange={handleValueChange}
+              value={inputs.studentName}
+              name="studentName"
+              placeholder="이름"
+              style={{ width: '390px', height: '40px', borderRadius: '22px', margin: 'auto' }}
+            />
+          </div>
+          <div className="row">
+            <TextField
+              onChange={handleValueChange}
+              pattern={REG_EXP.Number}
+              value={inputs.classCode}
+              name="classCode"
+              placeholder="학원코드입력"
+              style={{ width: '390px', height: '40px', borderRadius: '22px', margin: 'auto' }}
+            />
+          </div>
+          <div className="row justify-content-center">
+            <Button
+              onClick={handleSubmit}
+              ref={loginButton}
+              type="submit"
+              style={{
+                width: '390px',
+                height: '40px',
+                backgroundColor: '#e4cd84',
+                border: 'none',
+                borderRadius: '22px',
+              }}>
+              <span className="button-text">로그인</span>
+            </Button>
+          </div>
         </div>
       </div>
       <style jsx>
         {`
+          .bg {
+            display: flex;
+            background-image: url(${backgroundImg});
+            height: 100vh;
+            background-size: cover;
+          }
+          .testy-img {
+            margin-bottom: 40px;
+          }
           p {
             text-align: center;
-            font-family: NotoSansCJKkr;
             font-size: 20px;
             line-height: 1.45;
             letter-spacing: -1px;
@@ -87,6 +98,10 @@ const Login = () => {
           .row:nth-child(3) {
             margin-top: 10px;
             margin-bottom: 48px;
+          }
+          .button-text {
+            color: #4ca2c5;
+            font-weight: bold;
           }
         `}
       </style>
--- web/src/pages/Welcome.js
+++ web/src/pages/Welcome.js
@@ -1,75 +1,109 @@
 import React from 'react';
 import Button from '@/components/Button';
+import backgroundImg from '@/assets/images/background.png';
 
 const Welcome = () => {
   const handleStartButton = (e) => {
     e.preventDefault();
   };
   return (
-    <div className="container">
-      <div className="profileCircle" />
-      <div className="col">
-        <div className="row">
-          <div className="title">홍길동 학생, 어서오세요!</div>
-        </div>
-        <div className="row ">
-          <div>CLASS 둘리반 심화</div>
-        </div>
-        <div className="row">
-          <div>시험명 국어 {'>'} 문법 심화</div>
-        </div>
-        <div className="row">
-          <div className="Time">시험시간 60분</div>
-        </div>
-        <div className="row">
-          <Button
-            onClick={handleStartButton}
-            type="submit"
-            style={{
-              width: '354px',
-              height: '43px',
-              backgroundColor: '#e4cd84',
-              border: 'none',
-              borderRadius: '22px',
-            }}>
-            <span style={{ color: '#ffffff', fontWeight: 'bold' }}>시험시작</span>
-          </Button>
+    <div className="bg">
+      <div className="container">
+        <div className="profile-circle" />
+        <div className="col">
+          <div className="row">
+            <div className="title">홍길동 학생, 어서오세요!</div>
+          </div>
+          <div className="row">
+            <div className="class-title">CLASS</div>
+            <div>둘리반 심화</div>
+          </div>
+          <div className="row">
+            <div className="test-name">시험명</div>
+            <div>국어 {'>'} 문법 심화</div>
+          </div>
+          <div className="row">
+            <div className="test-time">시험시간</div>
+            <div>60분</div>
+          </div>
+          <div className="row">
+            <Button
+              onClick={handleStartButton}
+              type="submit"
+              style={{
+                width: '345px',
+                height: '43px',
+                backgroundColor: '#e4cd84',
+                border: 'none',
+                borderRadius: '22px',
+              }}>
+              <span className="button-text">시험시작</span>
+            </Button>
+          </div>
         </div>
       </div>
       <style jsx>
         {`
+          .bg {
+            display: flex;
+            background-image: url(${backgroundImg});
+            height: 100vh;
+            background-size: cover;
+          }
           .container {
             margin-top: 120px;
-            width: 430px;
+            width: 350px;
           }
-          .profileCircle {
-            border: 1px solid;
+          .profile-circle {
             width: 100px;
             height: 100px;
             border-radius: 75px;
             margin: 0 auto;
+            background-color: #ffffff;
           }
-          .row {
+          .row:nth-child(1) {
             justify-content: center;
+          }
+          .row:nth-child(3) {
+            line-height: 1.46;
+            letter-spacing: -1.3px;
+            color: #ffffff;
+            white-space: pre;
+          }
+          .class-title {
+            margin-left: 40px;
+            margin-right: 70px;
+          }
+          .test-name {
+            margin-left: 48px;
+            margin-right: 70px;
+          }
+          .test-time {
+            margin-left: 33px;
+            margin-right: 63px;
           }
           .row:nth-child(2n) {
             background-color: #cee1e9;
             height: 43px;
-            padding: 10px;
-            margin: 8px;
+            padding: 8px;
+            margin-top: 8px;
+            margin-bottom: 10px;
+            color: #707070;
           }
           .row:nth-child(5) {
             margin-top: 50px;
           }
           .title {
             padding-top: 28px;
-            padding-bottom: 40px;
-            font-family: NotoSansCJKkr;
-            font-size: 20px;
+            padding-bottom: 30px;
+            font-size: 22px;
             font-weight: bold;
             line-height: 1.47;
             letter-spacing: -1.7px;
-            color: black;
+            color: #ffffff;
+          }
+          .button-text {
+            font-weight: bold;
           }
         `}
       </style>
