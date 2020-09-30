5f544465092a107335f241c0374b61c684ff2ae8
JunKyus <kuk941025@gmail.com>
2020-09-30 22:44:17 +0900

    Add delete icon

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import AnswerInput from './AnswerInput';
 import QuestionType from './QuestionType';
+import DeleteIcon from '@/assets/images/delete.png';
 
-const Question = () => {
+const Question = ({ num = 0 }) => {
   return (
     <div className="question-root">
       <QuestionType />
 
-      <div className="row input">
+      <p className="title">문제 {num}</p>
+      <div className="row">
         <div className="col-8">
-          <div className="question-image">이미지를 첨부하세요</div>
+          <div className="question-image">
+            <div>
+              <span className="add">+</span>
+              <p>이미지를 첨부하세요.</p>
+            </div>
+            <img className="delete" src={DeleteIcon} />
+          </div>
         </div>
         <div className="col-4">
           <AnswerInput />
@@ -17,6 +25,9 @@ const Question = () => {
       </div>
 
       <style jsx>{`
+        .add { 
+          font-size: 28px;
+        }
         .question-root {
           width: 100%;
           padding-bottom: 5%;
@@ -29,9 +40,23 @@ const Question = () => {
           align-items: center;
           justify-content: center;
           border: solid 1px #707070;
+          color: #707070;
+          text-align: center;
         }
-        .input { 
-          margin-top: 24px;
+        .question-image > div {
+          cursor: pointer;
+        }
+        .question-image .delete {
+          position: absolute;
+          right: 32px;
+          bottom: 16px;
+          cursor: pointer;
+        }
+        .title {
+          margin-top: 20px;
+          font-weight: 600;
+          color: #707070;
+          font-size: 18px;
         }
       `}</style>
     </div>
