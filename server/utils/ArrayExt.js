392c17134af6a412f32379029ff6471f083c191a
yoonseo <nicealice99@gmail.com>
2020-10-15 21:45:23 +0900

    delete files

--- server/utils/ArrayExt.js
+++
@@ -1,9 +0,0 @@
-const asyncForEach = async (array, callback) => {  // 비동기 방식 ForEach
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
-module.exports = {
-  asyncForEach
-}
